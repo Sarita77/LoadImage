6a47d81827116409ae2e9e0a57e91471308bb117
Sarita Asthani <asthani77@outlook.com>
2020-01-05 17:51:19 +0545

    correction made

--- app/src/main/java/com/example/loadimage/MainActivity.java
+++ app/src/main/java/com/example/loadimage/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.loadimage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    RadioButton rdoDaya, rdoRajesh, rdoBhuwan;
+    ImageView imgHero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rdoDaya = findViewById(R.id.rdoDaya);
+        rdoRajesh = findViewById(R.id.rdoRajesh);
+        rdoBhuwan = findViewById(R.id.rdoBhuwan);
+        imgHero = findViewById(R.id.imgHero);
+
+        rdoDaya.setOnClickListener(this);
+        rdoRajesh.setOnClickListener(this);
+        rdoBhuwan.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.rdoDaya:
+                imgHero.setImageResource(R.drawable.dayahang);
+                break;
+            case R.id.rdoRajesh:
+                imgHero.setImageResource(R.drawable.rajesh);
+                break;
+            case R.id.rdoBhuwan:
+                imgHero.setImageResource(R.drawable.bhuwan);
+                break;
+        }
+
     }
 }
